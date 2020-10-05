f3ed11d3a44892cb4fcf20301cbd4cea8edb0292
dongwkim <dongwkim@student.42seoul.kr>
2020-10-05 17:40:22 +0900

    20201005 update

---
+++ dongwkim/1주차/A+B-8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, a, b;
+
+    cin >> n;
+    for(int i = 1;i <= n;i++){
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a << " + " << b << " = " << a + b << endl;
+    }
+}
---
+++ dongwkim/1주차/빠른A+B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ios>
+
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n, a, b;
+    cin >> n;
+    
+    for (int i = 0;i < n;i++){
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+}
