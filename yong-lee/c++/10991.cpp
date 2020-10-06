8d19268b87c874ed2681560cd143e294a49e08ad
dldydrhkd <48857296+dldydrhkd@users.noreply.github.com>
2020-10-06 16:29:05 +0900

    별 찍기 - 16

--- yong-lee/c++/10991.cpp
+++ yong-lee/c++/10991.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-int n;
-int arr[1000001];
 
 int main(){
-    cin>>n;
-    for(int i=0; i<n; i++){
-        cin >>arr[i];
+    int n;
+    cin >>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+            cout<<" ";
+        }
+        for(int j=1; j<=i; j++){
+            cout<<"* ";
+        }
+        cout<<"\n";
     }
-    sort(arr,arr+n);
-
-    cout<<arr[0]<<" "<<arr[n-1];
 }
