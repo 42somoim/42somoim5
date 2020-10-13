1a9dd29a6199a3cd41eed29adc5f7576d088af43
Kyle <mkitigy@gmail.com>
2020-10-13 18:52:28 +0900

    add 11053.cpp

---
+++ gim/week_2/11053.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(void)
+{
+    int N, max_l, result;
+    int arr[1001];
+    int len[1001];
+
+    cin >> N;
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+    result = 0;
+    for (int i = 0; i < N; i++)
+    {
+        max_l = 0;
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] < arr[i])
+                max_l = max(max_l, len[j]);
+        }
+        len[i] = max_l + 1;
+        result = len[i] > result ? len[i] : result;
+    }
+    cout << result << '\n';
+    return (0);
+}
