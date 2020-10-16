fc01dc36468fa9004537728c7f536bc952649be9
Gwang yeol Im <gim@c6r7s2.42seoul.kr>
2020-10-16 15:11:12 +0900

    add gim

--- gim/week_2/2579.cpp
+++ gim/week_2/2579.cpp
@@ -14,8 +14,10 @@ int main(void)
     for (int i = 1; i <= N; i++)
         scanf("%d", &stairs[i]);
     dp[1] = stairs[1];
-    for (int i = 3; i <= N; i++)
-        dp[i] = max(dp[i - 3] + stairs[i - 1] + stairs[i], dp[i - 2], stairs[i]);
+    dp[2] = stairs[1] + stairs[2];
+    dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3]);
+    for (int i = 4; i <= N; i++)
+        dp[i] = max(dp[i - 3] + stairs[i - 1] + stairs[i], dp[i - 2] + stairs[i]);
     printf("%d\n", dp[N]);
     return (0);
 }
