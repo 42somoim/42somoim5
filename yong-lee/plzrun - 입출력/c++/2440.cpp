5faa564fc390a9b8a77f6db743e5f477b6e4e9d9
dldydrhkd <48857296+dldydrhkd@users.noreply.github.com>
2020-10-17 11:30:45 +0900

    별 찍기 - 3

---
+++ yong-lee/plzrun - 입출력/c++/2440.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        for (int j = n-i; j >= 0; j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+}
