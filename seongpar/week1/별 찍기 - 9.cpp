ad27884593af9fb0cb0b7d7a57227ff6b76e8a76
mimseong <parkseongmin98@gmail.com>
2020-10-18 15:42:37 +0900

    add week1

---
+++ seongpar/week1/별 찍기 - 9.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <functional>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <cmath>
+#include <cstring>
+#include <bitset>
+
+#define xx first
+#define yy second
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+using i64 = long long int;
+using ii = pair<int, int>;
+using ii64 = pair<i64, i64>;
+using iii = tuple<int, int, int>;
+
+using namespace std;
+
+int     main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = i; j < n; j++)
+            printf(" ");
+        for (int j = 0; j < 2 * i - 1; j++)
+            printf("*");
+        printf("\n");
+    }
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = i; j < n; j++)
+            printf(" ");
+        for (int j = 0; j < 2 * i - 1; j++)
+            printf("*");
+        printf("\n");
+    }
+
+    return 0;
+}
