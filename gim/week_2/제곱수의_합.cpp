ff9d0bc472056661f68a07d9f1549c2f33ec437a
Kyle <mkitigy@gmail.com>
2020-10-21 13:25:40 +0900

    Add gim

---
+++ gim/week_2/제곱수의_합.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int main(void) {
+    int dp[100001];
+    int N, min_n;
+
+    scanf("%d", &N);
+    dp[1] = 1;
+    for (int i = 2; i <= N; i++)
+    {
+        min_n = dp[i - 1];
+        for (int j = 2; j * j <= i; j++)
+            min_n = min(min_n, dp[i - (j * j)]);
+        dp[i] = min_n + 1;
+    }
+    printf("%d\n", dp[N]);
+    return (0);
+}
