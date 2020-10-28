28d954c1c694366c512f7e1f76e6ca6babea813a
ruzhub <ruz@outlook.kr>
2020-10-29 02:36:30 +0900

    refoldering

--- dju/week3/단어 길이 재기.cc
+++
@@ -1,7 +0,0 @@
-#include<cstdio>
-char a[101],i;
-int main() {
-	scanf("%s",a);
-	while (a[i++]);
-	printf("%d",i-1);
-}
--- dju/week3/알파벳 개수.cc
+++
@@ -1,9 +0,0 @@
-#include<cstdio>
-
-char w[101];
-int ab[26];
-int main() {
-	scanf("%s", w);
-	for (int i = 0; w[i]; ++i) ab[w[i] - 'a']++;
-	for (int i = 0; i < 26; ++i) printf("%d ", ab[i]);
-}
